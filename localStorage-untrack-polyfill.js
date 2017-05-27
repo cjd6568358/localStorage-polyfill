3794e0a1a27562ffdd20fabfc8ef6c867006ef9e
cjd6568358 <cjd6568358@163.com>
2017-05-27 19:22:56 +0800

    fix ie8 字符集越界

--- localStorage-untrack-polyfill.js
+++ localStorage-untrack-polyfill.js
@@ -4,7 +4,7 @@
         var ip =
             /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
         if (ip.test(host) == true || host == 'localhost') return host;
-        var regex = /([^]*).*/;
+        var regex = /([^/]*).*/;
         var match = host.match(regex);
         if (typeof match !=
             "undefined" && null != match) host = match[1];
